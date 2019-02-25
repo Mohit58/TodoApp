f8008d659ff35d39cff3a845a39a316a1ab2c36f
mohit58 <mk581991@gmail.com>
2019-02-25 17:35:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
+import AddTodo from './addTodo';
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'Milk'},
+      {id: 2, content: 'Bred'},
+      {id: 3, content: 'Juice'},
+      {id: 4, content: 'Peanuts'},
+      {id: 5, content: 'Paneer'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    console.log(id);
+    const todos = this.state.todos.filter( todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos : todos
+    })
+  }
+
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos : todos
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
---
+++ src/Todos.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+var styleClose = {
+    cursor : 'pointer'
+}
+const Todos = ({todos, deleteTodo}) => {
+
+    const todoList = todos.length ? (
+        todos.map(todo => {
+            return (
+                <div className="collection-item" key={todo.id}>
+                    <span>{todo.content}</span>
+                    <span><i className="material-icons right red-text" onClick={() => {deleteTodo(todo.id)}} style={styleClose}>close</i></span>
+                </div>
+            )
+        })
+    ) : (
+        <p className="center">You have no todo's left, Yay!!</p>
+    )
+    return (
+        <div className="todos collection">
+            {todoList}
+        </div>
+        
+    )
+}
+
+export default Todos;
